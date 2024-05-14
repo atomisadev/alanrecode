224286d00c5155ced2275c5179bc07d93378d336
atomisadev <atomdevelops@pm.me>
2024-05-14 18:13:17 -0400

    metadata

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,11 @@
 import Navbar from "@/components/dashboard/navbar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ALAN | Dashboard",
+  description:
+    "The first, and best way to convert any images into music, to experience music in a whole new way.",
+};
 
 export default function DashLayout({
   children,
